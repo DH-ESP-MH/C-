1a5d3fc562746f24fca0cb7c3bd92daca7a85572
DH-ESP-MH <91718069+DH-ESP-MH@users.noreply.github.com>
2022-11-22 02:38:11 +0200

    1

---
+++ 05.09-11.09.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+#include <math.h>
+
+int main() {
+  double PI = 2 * acos(0.0);
+  int a = 3;
+  double b = 0.521;
+  std::cout << cbrt(
+      pow((0, 127 * exp(b / a)) / 1 - cbrt(cos((b / a) * PI)), 2));
+}
